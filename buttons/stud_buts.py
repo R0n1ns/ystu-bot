67520c568b05086484ca28fb47b296356af12856
R0n1n <korobovvad@mail.ru>
2024-03-03 20:32:22 +0300

    добавлены основные меню

--- buttons/stud_buts.py
+++ buttons/stud_buts.py
@@ -2,10 +2,11 @@ from aiogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 #кнопка основного меню студента
-main_stud_=[["Расписание","scheld"],["Оставить отзыв","revie"],["Соц сети","soc_net"]]
+main_stud_=[["Расписание","scheld"],["Отзывы","revie"],["Соц сети","soc_net"],["О вузе","news"]]
 #инциаизация клавиатуры
 main_stud_buts = InlineKeyboardBuilder()
 #генерация клавиатуры
 main_stud_buts.row(InlineKeyboardButton(text=main_stud_[0][0],callback_data=main_stud_[0][1]))
 main_stud_buts.row(InlineKeyboardButton(text=main_stud_[1][0],callback_data=main_stud_[1][1]))
-main_stud_buts.add(InlineKeyboardButton(text=main_stud_[2][0],callback_data=main_stud_[2][1]))
+main_stud_buts.row(InlineKeyboardButton(text=main_stud_[2][0],callback_data=main_stud_[2][1]))
+main_stud_buts.add(InlineKeyboardButton(text=main_stud_[3][0],callback_data=main_stud_[3][1]))
