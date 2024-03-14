a3b7c90b9ef8325c65d30f3dc7838d5ff0716c48
Zimmy <69210665+ZimmyCorp@users.noreply.github.com>
2024-03-15 00:21:13 +0300

    добавлен вывод соцсетей
    при получении расписания возобновляется меню студента/абитуриента, в зависимости от режима

--- handlers/abb_hand.py
+++ handlers/abb_hand.py
@@ -1,5 +1,7 @@
 import asyncio
 from aiogram import types,Router,F
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from aiogram.filters.command import Command
 #импорт кнопок
 from buttons.abb_buts import *
@@ -9,5 +11,16 @@ add_rout = Router()
 @add_rout.callback_query(F.data == "abb_mod")
 async def main_stud(callback: types.CallbackQuery):
     await callback.message.answer(text='Привет аббитуриент\nРадоваться рано,ты еще не поступил.',
+                                  reply_markup=main_abb_buts.as_markup())
+    await callback.message.delete()
+
+
+#вывод соцсетей
+@add_rout.callback_query(F.data == "soc_net_abb")
+async def soc_net(callback: types.CallbackQuery):
+    await callback.message.answer(text='Социальные сети:\n\n'
+                                       'VK: https://vk.com/ystu\n'
+                                       'Telegram: @YaroslavlSTU',
+                                  disable_web_page_preview=True,
                                   reply_markup=main_abb_buts.as_markup())
     await callback.message.delete()
