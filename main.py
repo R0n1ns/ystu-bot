364e8b15d7d04dd1cf89a5017c70b7df84eb6e96
R0n1n <korobovvad@mail.ru>
2024-05-02 10:20:36 +0300

    подкотовка

--- main.py
+++ main.py
@@ -9,7 +9,23 @@ from properties import token
 from tools.notif import notify
 
 # Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(level=logging.INFO)
+# set up logging to file
+logging.basicConfig(
+     filename='app.log',
+     level=logging.INFO,
+     format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+     datefmt='%H:%M:%S'
+ )
+
+# set up logging to console
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+logger = logging.getLogger(__name__)
 
 # Объект бота
 bot = Bot(token=token)
--- tools/notif.py
+++ tools/notif.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 from db.db import evd_notif, evd_notif_upd, evd_notif_send, evw_notif, evw_notif_send, evw_notif_upd, evl_notif, \
     evl_notif_upd, evl_notif_send
@@ -32,6 +33,7 @@ async def evl(time,bot):
         time_next =  i[0] - time
         await asyncio.sleep(time_next)
         users = await evl_notif_send()
+        logging.info(f"Отправлено на пару в {i[1]} уведомлений {len(users)}")
         for user in users:
             text = dict(eval(user["l_sch"]))
             if i[1] in text.keys():
@@ -56,6 +58,7 @@ async def evd(time,bot):
     time_next = evd_time*3600 - time
     await asyncio.sleep(time_next)
     users = await evd_notif_send()
+    logging.info(f"Отправлено ежедневных уведомлений {len(users)}")
     for user in users:
         await bot.send_message(user["id_tg"], user["d_sch"])
 #############################################################
@@ -81,6 +84,7 @@ async def evw(time,bot):
     time_next = evw_time*3600 - time
     await asyncio.sleep(time_next)
     users = await evw_notif_send()
+    logging.info(f"Отправлено еженедельных уведомлений {len(users)}")
     for user in users:
         await bot.send_message(user["id_tg"], user["w_sch"])
 #############################################################
@@ -91,9 +95,8 @@ async def notify(bot):
     time = datetime.now()
     time = time.second + time.minute * 60 + time.hour * 3600
     next_time = 24 * 3600 - time
-    print(next_time/3600)
+    logging.info(f"След. время обновления расписания через {next_time/3600}")
     await asyncio.sleep(next_time)
-
     #ежедневные уведомления
     await evd_sch()
     await evd(time,bot)
@@ -104,7 +107,7 @@ async def notify(bot):
     if time_w ==1:
         await evw_sch()
         await evw(time,bot)
-
+    logging.info(f"Уведомления успешно отправлены")
     #заново
     await notify(bot)
 #
